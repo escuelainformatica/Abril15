5683629562b5406b400615880e6b6d004a0922a7
Jorge Castro <jorge.castro.c@gmail.com>
2020-04-15 19:35:46 -0400

    primera version

--- Abril15-ejb/src/java/cl/cocacola/ejb/FacturaEJB.java
+++ Abril15-ejb/src/java/cl/cocacola/ejb/FacturaEJB.java
@@ -7,6 +7,7 @@ import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 // EJB para nosotros son las clases de servicio.
 // clases que tienen funciones solamente.
@@ -24,14 +25,17 @@ public class FacturaEJB {
     public int sumar(int n1,int n2) {
         return n1+n2;
     }
-    
     public List<Factura> listar() {
         return em
                 .createQuery("select f from Factura f") // JPQL
                 .getResultList();
     }
     
+    //  EJB todos los metodos son transacciones.
+    // que significa. Si una operacion falla, se cancela todas las operaciones    
+    // @Transactional
+    
     public void insertar(Factura factura) {
-        em.persist(factura);
+        em.persist(factura); // save()
     }
 }
